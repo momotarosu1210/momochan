5ac3e66e055302000179d928c9084e97202c51bc
momotarosu1210 <56685454+momotarosu1210@users.noreply.github.com>
2019-11-27 20:33:47 +0900

    Add files via upload

--- barger.js
+++ barger.js
@@ -1,7 +1,7 @@
 $(function() {
  $('.Toggle,li > a').click(function() {
   $('.Toggle').toggleClass('active');
-  $('body').toggleClass('fixed')
+  $('body').toggleClass('fixed');
 
  if ($('.Toggle').hasClass('active')) {
   $('.NavMenu').addClass('active'); //クラスを付与
